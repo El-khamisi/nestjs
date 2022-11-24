365c80a51b0e74a4b723c8f32d9599a71894d2e5
El-khamisi <El-khamisi@users.noreply.github.com>
2022-11-24 14:18:18 +0000

    Apply Lint&&Prettier Formatting

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() 
-{
-
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(5050);
-  const env =   app.get(ConfigService);
+  const env = app.get(ConfigService);
   console.log(env.get('DATABASE_URL'));
   console.log(env.get('cup'));
 }
