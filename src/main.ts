adbed4cfb7a1aa31a568b09a4d36092ded928a55
El-khamisi <mohammd.alkhamisi@gmail.com>
2022-11-24 09:19:01 +0200

    actions@

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.DATABASE_URL)
-  console.log(process.env.cup)
   await app.listen(5050);
+  const env = app.get(ConfigService);
+  console.log(env.get('DATABASE_URL'))
+  console.log(env.get('cup'))
 }
 bootstrap();
