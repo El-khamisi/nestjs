565dd16aea9c81fc2dfea025b7aa51bcb714ab3f
El-khamisi <mohammd.alkhamisi@gmail.com>
2022-11-24 16:24:41 +0200

    Revert "Apply Lint&&Prettier Formatting"
    This reverts commit 365c80a51b0e74a4b723c8f32d9599a71894d2e5.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap() 
+{
+
   const app = await NestFactory.create(AppModule);
   await app.listen(5050);
-  const env = app.get(ConfigService);
+  const env =   app.get(ConfigService);
   console.log(env.get('DATABASE_URL'));
   console.log(env.get('cup'));
 }
